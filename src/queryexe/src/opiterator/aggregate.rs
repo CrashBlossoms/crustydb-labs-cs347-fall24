294ec927298d34d1e7cdacfbd440540a09ac369f
Alex Chavez <achavez@cs.utexas.edu>
2024-11-16 18:40:59 -0600

    passes min test in agg

--- src/queryexe/src/opiterator/aggregate.rs
+++ src/queryexe/src/opiterator/aggregate.rs
@@ -3,6 +3,7 @@ use crate::Managers;
 use common::bytecode_expr::ByteCodeExpr;
 use common::datatypes::f_decimal;
 use common::{AggOp, CrustyError, Field, TableSchema, Tuple};
+use sqlparser::keywords::PERCENTILE_CONT;
 use std::cmp::{max, min};
 use std::collections::HashMap;
 
@@ -27,6 +28,11 @@ pub struct Aggregate {
     
     // States (Need to reset on close)
     // todo!("Your code here")
+    // Aggregation state
+    agg_results: HashMap<Vec<Field>, Vec<Field>>, // Maps group keys to aggregate values
+    count_map: HashMap<Vec<Field>, usize>,        // Tracks counts for avg operations
+    result_tuples: Vec<Tuple>,                    // Stores finalized aggregation results
+    result_index: usize,                          // Index to track position in result_tuples for iteration
 }
 
 impl Aggregate {
@@ -39,9 +45,23 @@ impl Aggregate {
         child: Box<dyn OpIterator>,
     ) -> Self {
         assert!(ops.len() == agg_expr.len());
-        todo!("Your code here")
+        // todo!("Your code here")
+        Self {
+            managers,
+            groupby_expr,
+            agg_expr,
+            ops,
+            schema,
+            child,
+            will_rewind: false,
+            agg_results: HashMap::new(),
+            count_map: HashMap::new(),
+            result_tuples: Vec::new(),
+            result_index: 0,
+        }
     }
 
+    //Self::merge_fields(*op, &field_val, &mut agg_values[i])?;
     fn merge_fields(op: AggOp, field_val: &Field, acc: &mut Field) -> Result<(), CrustyError> {
         match op {
             AggOp::Count => *acc = (acc.clone() + Field::Int(1))?,
@@ -51,7 +71,7 @@ impl Aggregate {
             }
             AggOp::Min => {
                 let min = min(acc.clone(), field_val.clone());
-                *acc = min;
+                *acc = min; //maybe use an IF statement here?
             }
             AggOp::Sum => {
                 *acc = (acc.clone() + field_val.clone())?;
@@ -63,9 +83,59 @@ impl Aggregate {
         Ok(())
     }
 
-    pub fn merge_tuple_into_group(&mut self, tuple: &Tuple) {
-        todo!("Your code here");
+    pub fn merge_tuple_into_group(&mut self, tuple: &Tuple) -> Result<(), CrustyError> {
+
+        // Step 1: Calculate the group key by evaluating group-by expressions
+        let group_key: Vec<Field> = self
+            .groupby_expr
+            .iter()
+            .map(|expr| expr.eval(tuple))
+            .collect();
+    
+        // Step 2: Initialize aggregation entry if the group key is new
+        if !self.agg_results.contains_key(&group_key) {
+            
+            // Initialize aggregation vector with default values (e.g., 0 for Sum, Min, etc.)
+            let initial_agg_values = self
+                .ops
+                .iter()
+                .map(|op| match op {
+                    AggOp::Count => Field::Int(0),
+                    AggOp::Sum | AggOp::Avg => Field::Int(0), // Use 0 initially for Sum and Avg
+                    AggOp::Min => Field::Null,       // Initialize Min to max possible value
+                    AggOp::Max => Field::Null,       // Initialize Max to min possible value
+                })
+                .collect();
+    
+            self.agg_results.insert(group_key.clone(), initial_agg_values); //agg_results holds default values for this key now
+            self.count_map.insert(group_key.clone(), 0);
+        }
+    
+        // Step 3: For each aggregation operation, update the corresponding field in agg_results
+        let agg_values = self.agg_results.get_mut(&group_key).unwrap(); //problem originates around here! agg_values is wrong
+
+        for (i, op) in self.ops.iter().enumerate() {
+
+            let field_val = self.agg_expr[i].eval(tuple);
+            // println!("CURRENT field val is {}", &field_val);
+            // println!("CURRENT field val is {}", &mut agg_values[i]); //agg_values[i] has max value
+
+            // Merge field value into aggregate using the specified operation
+            Self::merge_fields(*op, &field_val, &mut agg_values[i])?;
+        }
+
+        println!("agg_values is {:?}", agg_values);
+    
+        // Step 4: Update count for Avg operations
+        if self.ops.contains(&AggOp::Avg) {
+            *self.count_map.get_mut(&group_key).unwrap() += 1;
+        }
+    
+        Ok(())
     }
+    
+
+
 }
 
 impl OpIterator for Aggregate {
@@ -76,19 +146,94 @@ impl OpIterator for Aggregate {
     }
 
     fn open(&mut self) -> Result<(), CrustyError> {
-        todo!("Your code here")
+        //ppen the child operator
+        self.child.open()?;
+    
+        // get all tuples from child and sort them into groups
+        while let Some(tuple) = self.child.next()? {
+            self.merge_tuple_into_group(&tuple)?;
+        }
+    
+        // Finalize the aggregated results
+        self.result_tuples.clear();
+        for (group_key, agg_values) in &self.agg_results {
+            let mut final_tuple = group_key.clone(); // Start with group-by fields
+    
+            for (i, field) in agg_values.iter().enumerate() {
+                
+                // Finalize average calculation if required
+                println!("PRINTING OP {}", self.ops[i]);
+                let final_field = if self.ops[i] == AggOp::Avg { //final field is incorrect!! could be a string!!
+                    let count = *self.count_map.get(group_key).unwrap_or(&1) as i64;
+                    if let Field::Int(sum) = field {
+                        Field::Int(sum / count)
+                    } else {
+                        return Err(CrustyError::CrustyError("Expected Int field for Avg".to_string()));
+                    }
+                } else {
+                    println!("final field is not avg");
+                    println!("field is: {}", field);
+                    field.clone()
+                };
+    
+                final_tuple.push(final_field);
+            }
+            // println!("PRINTING IN OPEN {:?}", final_tuple);
+            // Field::to_bytes(&final_tuple); 
+
+            //final tuple is vec of fields
+            //we need to turn this into a tuple
+
+            //Tuple can be created from a vec of Fields
+
+            self.result_tuples.push(Tuple::new(final_tuple)); // Convert Vec<Field> to Tuple  COULD BE AN ISSUE
+        }
+    
+        // Reset result index for iteration
+        self.result_index = 0;
+    
+        Ok(())
     }
+    
 
     fn next(&mut self) -> Result<Option<Tuple>, CrustyError> {
-        todo!("Your code here")
+
+        // if self.child == None {
+        //     panic!()
+        // }
+        
+        // Check if there are more tuples to return
+        if self.result_index < self.result_tuples.len() {
+            // Get the next tuple and increment the index
+            let next_tuple = self.result_tuples[self.result_index].clone();
+            self.result_index += 1;
+            // println!("PRINTING IN NEXT() {}", next_tuple);
+            Ok(Some(next_tuple)) //next gives a vector of tuples
+        } else {
+            // No more tuples to return
+            Ok(None)
+        }
+        //todo
     }
 
     fn close(&mut self) -> Result<(), CrustyError> {
-        todo!("Your code here")
+        
+        // Close the child operator
+        self.child.close()?;
+    
+        // Clear aggregation state
+        self.agg_results.clear();
+        self.count_map.clear();
+        self.result_tuples.clear();
+        self.result_index = 0;
+    
+        Ok(())
     }
 
     fn rewind(&mut self) -> Result<(), CrustyError> {
-        todo!("Your code here")
+        //close the current state and reopen
+        self.close()?;
+        self.open()
     }
 
     fn get_schema(&self) -> &TableSchema {
